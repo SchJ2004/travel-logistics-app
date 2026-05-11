d96fa357b6e13933f10183774e7dc5db14fbe028
schj2004 <schultzj04@gmail.com>
2026-05-11 15:37:08 -0400

    Built Passenger Details screen and integrated live Duffel API checkout flow

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3149754869" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "975756874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
